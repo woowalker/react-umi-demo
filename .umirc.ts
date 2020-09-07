a0ed9b0f5a419a28359c3174041d694b3920fa08
xiongqiang <qxiong@optikom.cn>
2020-09-07 14:13:26 +0800

    feat: 优化 .umirc.js 配置文件

--- .umirc.ts
+++ .umirc.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
-  nodeModulesTransform: {
-    type: 'none',
-  },
+  // 路由
+  routes: [
+    { exact: true, path: '/', redirect: '/home' },
+    { path: '/home', component: '@/pages/home' },
+    { path: '/dashboard', component: '@/pages/dashboard' },
+  ],
+  // 按需加载配置
   dynamicImport: {
     loading: '@/components/layout/PageLoading',
+  },
+  nodeModulesTransform: {
+    type: 'none',
   },
 });
--- config/config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'umi';
-import router from '@/router.js';
-
-export default defineConfig({
-  routes: router,
-});
